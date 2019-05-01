87ee5d1952f9958fac4472258a3b4b309d8f1f80
dbanadol <dbanadol94@gmail.com>
2019-05-01 23:12:07 +0300

    test

--- app/src/main/java/com/example/babu/MainActivity.java
+++ app/src/main/java/com/example/babu/MainActivity.java
@@ -28,7 +28,7 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageButton;
